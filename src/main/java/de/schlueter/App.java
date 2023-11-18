bddfd50022cf7452d83c72dc623c478fe452b9f0
Maduki-tech <d.schlueter1011@gmail.com>
2023-11-18 20:43:25 +0100

    initial project setup

---
+++ src/main/java/de/schlueter/App.java
@@ -0,0 +1,13 @@
+package de.schlueter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
